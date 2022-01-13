3421e0b5157929adb5ebab19c5b4eb904e54584f
E ZED <hzhang83@gmail.com>
2022-01-13 14:25:45 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,48 @@
+import "./styles.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createMachine, assign } from "xstate";
+import { useMachine } from "@xstate/react";
+
+interface ToggleContext {
+  count: number;
+}
+
+const toggleMachine = createMachine<ToggleContext>({
+  id: "toggle",
+  initial: "inactive",
+  context: {
+    count: 0
+  },
+  states: {
+    inactive: {
+      on: { TOGGLE: "active" }
+    },
+    active: {
+      entry: assign({ count: (ctx) => ctx.count + 1 }),
+      on: { TOGGLE: "inactive" }
+    }
+  }
+});
+
+function App() {
+  const [current, send] = useMachine(toggleMachine);
+  const active = current.matches("active");
+  const { count } = current.context;
+
+  return (
+    <div className="App">
+      <h1>XState React Template</h1>
+      <h2>Fork this template!</h2>
+      <button onClick={() => send("TOGGLE")}>
+        Click me ({active ? "✅" : "❌"})
+      </button>{" "}
+      <code>
+        Toggled <strong>{count}</strong> times
+      </code>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
